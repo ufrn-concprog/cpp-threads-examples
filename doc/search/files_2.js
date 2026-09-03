da4445f1967feacd6ed48b1f55fe5f1fae9bc824
Everton Cavalcante <evertonranielly@gmail.com>
2026-09-02 23:23:16 -0300

    Updated Doxygen documentation

--- doc/search/files_2.js
+++ doc/search/files_2.js
@@ -4,5 +4,6 @@ var searchData=
   ['thread_2dclass_2ecpp_1',['thread-class.cpp',['../thread-class_8cpp.html',1,'']]],
   ['thread_2dclass2_2ecpp_2',['thread-class2.cpp',['../thread-class2_8cpp.html',1,'']]],
   ['thread_2did_2ecpp_3',['thread-id.cpp',['../thread-id_8cpp.html',1,'']]],
-  ['thread_2dsleep_2ecpp_4',['thread-sleep.cpp',['../thread-sleep_8cpp.html',1,'']]]
+  ['thread_2dlambda_2ecpp_4',['thread-lambda.cpp',['../thread-lambda_8cpp.html',1,'']]],
+  ['thread_2dsleep_2ecpp_5',['thread-sleep.cpp',['../thread-sleep_8cpp.html',1,'']]]
 ];
